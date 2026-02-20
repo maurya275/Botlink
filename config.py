a0198e461d846033c6510313ea1365d5f6072b22
maurya275 <mauryavivek89074@gmail.com>
2026-02-20 17:08:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,29 @@
-# Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
-
 import re
 
-API_ID = "30861624" # Your Telegram API ID
-API_HASH = "4bac8ea697e16d2323ad170a04aa552a" # Your Telegram API Hash
-BOT_TOKEN = "8383893227:AAGoFsjWvYblmQJJnbQuA0eBJIBORYyR6fI" # Your Bot Token
+API_ID = "PASTE_NEW_API_ID"
+API_HASH = "PASTE_NEW_API_HASH"
+BOT_TOKEN = "PASTE_NEW_BOT_TOKEN"
 
-# MongoDB connection URI
-MONGO_URI = "mongodb+srv://Shizukamusicsss:Shizukamusicsss@cluster0.zupd3uz.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://=Cluster0"
 
 DEFAULT_WARNING_LIMIT = 3
-DEFAULT_PUNISHMENT = "mute" # Options: "mute", "ban"
+DEFAULT_PUNISHMENT = "mute"
 DEFAULT_CONFIG = ("warn", DEFAULT_WARNING_LIMIT, DEFAULT_PUNISHMENT)
 
-# Regex pattern to detect URLs in user bios
+# Bio URL pattern
 URL_PATTERN = re.compile(
-    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*' #done change here
+    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*'
 )
+
+# Message Telegram link pattern
+TELEGRAM_LINK_PATTERN = re.compile(
+    r"(https?://)?(www\.)?(t\.me|telegram\.me)/[^\s]+",
+    re.IGNORECASE
+)
+
+PROMOTION_TEXT = (
+    "Member/Bot ke bheje gaye link ko delete kiya gaya.\n\n"
+    "Apne group ko secure karne ke liye hamen apne group me add kare."
+)
+
+BOT_USERNAME = "BotLinkRemoverBot"
