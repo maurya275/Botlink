0bfd93d09d91dafea23da05102e503404bc11f8f
maurya275 <mauryavivek89074@gmail.com>
2026-02-27 20:23:34 +0530

    Update bio.py

--- bio.py
+++ bio.py
@@ -76,7 +76,7 @@ async def start(_, message):
         "   └ Edited Link Detection\n\n"
         "🗑 𝗠𝗘𝗗𝗜𝗔 𝗖𝗟𝗘𝗔𝗡𝗨𝗣\n"
         "   └ Silent Auto Delete (50s)\n\n"
-        "⚡ Fast • Stable • Professional Grade Protection\n\n"
+        "⚡ Fast • Stable • Professional #Grade Protection\n\n"
         f"📢 Support Channel: {SUPPORT_CHANNEL}\n"
         f"👥 Support Group: {SUPPORT_GROUP}"
     )
